d5de910cd103b328bfac40dd65926748821cc7e3
coyla <jcoyla123@gmail.com>
2017-12-06 17:06:31 +0100

    Add post and get routes and bot returns response into json type

--- app.py
+++ app.py
@@ -1,52 +1,12 @@
-from flask import Flask, render_template, request, jsonify
-import atexit
-import json
-from cloudant import Cloudant
+from flask import Flask,request,jsonify
 from chatterbot import ChatBot
-import cf_deployment_tracker
 import os
 import logging
 
-
-cf_deployment_tracker.track()
-
 app = Flask(__name__)
+port = int(os.getenv('VCAP_APP_PORT', '8000'))
 
-db_name = 'mydb'
-client = None
-db = None
-
-if 'VCAP_SERVICES' in os.environ:
-    vcap = json.loads(os.getenv('VCAP_SERVICES'))
-    print('Found VCAP_SERVICES')
-    if 'cloudantNoSQLDB' in vcap:
-        creds = vcap['cloudantNoSQLDB'][0]['credentials']
-        user = creds['username']
-        password = creds['password']
-        url = 'https://' + creds['host']
-        client = Cloudant(user, password, url=url, connect=True)
-        db = client.create_database(db_name, throw_on_exists=False)
-elif os.path.isfile('vcap-local.json'):
-    with open('vcap-local.json') as f:
-        vcap = json.load(f)
-        print('Found local VCAP_SERVICES')
-        creds = vcap['services']['cloudantNoSQLDB'][0]['credentials']
-        user = creds['username']
-        password = creds['password']
-        url = 'https://' + creds['host']
-        client = Cloudant(user, password, url=url, connect=True)
-        db = client.create_database(db_name, throw_on_exists=False)
-
-# On Bluemix, get the port number from the environment variable PORT
-# When running this app on the local machine, default the port to 8000
-port = int(os.getenv('PORT', 8000))
-
-# On Bluemix, get the port number from the environment variable PORT
-# When running this app on the local machine, default the port to 8000
-#port = os.getenv('VCAP_APP_PORT', '5000')
-
-
-#Chatbot loading
+#Chatbot load
 logging.basicConfig(level=logging.INFO)
 def debug(message):
 	print("[debug] : %s" %(message))
@@ -75,14 +35,21 @@ debug("chatterbot is running  ...")
 
 #desactivation d'apprentissage automatique
 bot.read_only = True
-@app.route("/<content>")
-def message(content):
-	response = 'User said : ' + content
-
-	print(response)
+@app.route('/message',methods=['POST'])
+def post_message():
+	request_response = request.get_json(force=True,silent=True)
+	content = request_response['content'] #message from user
 	print('session ',bot.default_session.id_string)
-	bot_response = 'bot said : ' + bot.get_response(content).text
-	return bot_response
+	response_dict = {'message': bot.get_response(content).text }
+	return jsonify(response_dict)
+
+@app.route('/message',methods=['GET'])
+def get_message():
+	response_dict = {'message': bot.get_response('ITG').text }
+	response_json = jsonify(response_dict)
+	print(type(response_json))
+	return response_json
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=port, debug=True)
+
